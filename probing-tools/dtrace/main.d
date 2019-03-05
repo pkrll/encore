8fd1b5e14f14ab98476140763e1e4dcafe1c416f
Ardalan Samimi <ardalan.samimi@gmail.com>
2019-03-05 11:09:47 +0100

    Cleaned up some code and fixed the output formatting

--- probing-tools/dtrace/main.d
+++ probing-tools/dtrace/main.d
@@ -8,42 +8,7 @@
 #define ACTORMSG_CONF (4294967295 - 2)  	// 4294967293
 #define ACTORMSG_ACK (4294967295 - 1) 		// 4294967294
 
-typedef struct pony_ctx_t
-{
-  struct scheduler_t* scheduler;
-};
-
-struct scheduler_t
-{
-  uint32_t tid;
-  uint32_t cpu;
-};
-
-struct future_block {
-  uint64_t start;
-  uint64_t end;
-};
-
-struct future_fulfil {
-  uint64_t start;
-  uint64_t end;
-};
-
-struct future {
-  uintptr_t ptr;
-  uint64_t created;
-  uint64_t lifetime;
-  int get;
-  int chaining;
-
-  struct future_block blocking;
-  struct future_fulfil fulfil;
-};
-
-struct future futures[uintptr_t];
-
-int number_of_messages_sent;
-int number_of_futures;
+int did_run_probe[string];
 
 pony$target:::actor-alloc {
 	#if defined(DOPRINT) && DOPRINT == 1
@@ -51,29 +16,41 @@ pony$target:::actor-alloc {
   #endif
 }
 
+pony$target::: /did_run_probe[probename] != 1/ {
+  did_run_probe[probename] = 1;
+}
+
+encore$target::: /did_run_probe[probename] != 1/ {
+  did_run_probe[probename] = 1;
+}
+
 pony$target:::actor-msg-send {
-  @[probename] = count();
-  number_of_messages_sent += 1;
+  @counter[probename] = count();
 }
 
 encore$target:::future-create {
-  number_of_futures += 1;
-  future_created[arg1] = timestamp;
   @counter[probename] = count();
+  // Used for lifetime of a future
+  future_create_starttime[arg1] = timestamp;
 }
 
 encore$target:::future-block {
-  @counter[probename] = count();
-  future_blocked[arg1] = timestamp;
+  @counter[probename]   = count();
+  @future_block[arg1] = count();
+  @actor_blocked[arg0]  = count();
+  @future_blocked_actor[arg1, arg0] = count();
+  // Used for duration of a block
+  future_block_starttime[arg1] = timestamp;
 }
 
 encore$target:::future-unblock {
   @counter[probename] = count();
-  @future_blocked_lifetime["Future blocked duration", arg1] = sum(timestamp - future_blocked[arg1]);
+  @future_block_lifetime[arg1] = sum(timestamp - future_block_starttime[arg1]);
 }
 
 encore$target:::future-chaining {
   @counter[probename] = count();
+  @future_chaining[arg1] = count();
 }
 
 encore$target:::future-fulfil-start {
@@ -90,19 +67,40 @@ encore$target:::future-get {
 
 encore$target:::future-destroy {
   @counter[probename] = count();
-  @future_lifetime["Future lifetime", arg1] = sum(timestamp - future_created[arg1]);
-}
-
-encore$target:::method-entry {
-  // printf("\nMETHOD ENTRY: %s (%d)\n", copyinstr(arg2), arg2);
-  // ctx = (struct pony_ctx_t *)copyin(arg0, sizeof(struct pony_ctx_t));
-  // scd = (struct scheduler_t*)copyin((user_addr_t)ctx->scheduler, sizeof(struct scheduler_t));
-  // print(*ctx);
-  // print(*scd);
-  // printf("\n");
+  @future_lifetime[arg1] = sum(timestamp - future_create_starttime[arg1]);
 }
 
 END {
-  ratio = (number_of_futures > 0) ? number_of_messages_sent / number_of_futures : 0;
-  printf("Ratio: %d\n", ratio);
+  printf("=== FUTURE INFORMATION ===\n");
+  printf("=== COUNTS ===\n");
+  printa("%s\t%@1u\n", @counter);
+
+	if (did_run_probe["future-create"]) {
+	  printf("\n=== FUTURE_LIFETIME ===\n");
+	  printf("Future Addr\t\tLifetime (nanoseconds)\n");
+	  printa("%d\t\t%@1u\n", @future_lifetime);
+	}
+	if (did_run_probe["future-block"]) {
+	  printf("\n=== FUTURE_BLOCKED_LIFETIME ===\n");
+	  printf("Future Addr\t\tLifetime (nanoseconds)\n");
+	  printa("%d\t\t%@1u\n", @future_block_lifetime);
+
+  	printf("\n=== FUTURE_BLOCKED_ACTOR ===\n");
+  	printf("Future Addr\t\tActor addr\t\tLifetime (nanoseconds)\n");
+  	printa("%d\t\t%d\t\t%@2u\n", @future_blocked_actor);
+
+	  printf("\n=== NUMBER OF TIMES AN ACTOR IS BLOCKED ===\n");
+	  printf("Actor Addr\t\tCount\n");
+	  printa("%d\t\t%@2u\n", @actor_blocked);
+
+		printf("\n=== NUMBER OF TIMES A FUTURE BLOCKS ===\n");
+	  printf("Future Addr\t\tCount\n");
+	  printa("%d\t\t%@2u\n", @future_block);
+	}
+
+	if (did_run_probe["future-chaining"]) {
+	  printf("\n=== NUMBER OF TIMES A FUTURE IS CHAINED ===\n");
+	  printf("Future Addr\t\tCount\n");
+	  printa("%d\t\t%@2u\n", @future_chaining);
+	}
 }
